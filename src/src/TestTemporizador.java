50814f226d65350fe631e8ee624022dbbf1e0009
usuario <usuario@10.5.79.224>
2017-10-17 21:58:04 -0300

    17/10/2017

--- src/src/TestTemporizador.java
+++ src/src/TestTemporizador.java
@@ -1,31 +1,32 @@
 package src;
 
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
-public final class TestTemporizador  implements ActionListener{
+public class TestTemporizador implements ActionListener {
 
 	public static void main(String[] args) {
 
 		ActionListener objetInterfaz = new TestTemporizador();
-		
-		Timer temporizador = new Timer (2000, objetInterfaz);
-		
+
+		Timer temporizador = new Timer(1000, objetInterfaz);
+
 		temporizador.start();
-		
+
 		JOptionPane.showMessageDialog(null, "Aprete Aceptar para que se detenga el programa");
-	
+
 		System.exit(0);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
+
 		Date horaLocal;
 		System.out.println(horaLocal = new Date());
+		Toolkit.getDefaultToolkit().beep();
 	}
-
 }
